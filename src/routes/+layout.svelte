6f3b0beaff2ca756787024348ee31853ce029dd5
Kip <Kipling.Dunlap@gmail.com>
2024-02-01 21:19:34 -0500

    move font imports to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<slot />
+
+<style>
+  @font-face {
+    font-family: 'Tungsten-SemiBold';
+    src: url(./fonts/Tungsten-Semibold.otf) format('opentype');
+    letter-spacing: 2pt;
+  }
+
+  @font-face {
+    font-family: 'GT-Eesti-Regular';
+    src: url(./fonts/GT-Eesti-Display-Regular.otf) format('opentype');
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -974,17 +974,6 @@
 </footer>
 
 <style>
-  @font-face {
-    font-family: 'Tungsten-SemiBold';
-    src: url(./fonts/Tungsten-Semibold.otf) format('opentype');
-    letter-spacing: 2pt;
-  }
-
-  @font-face {
-    font-family: 'GT-Eesti-Regular';
-    src: url(./fonts/GT-Eesti-Display-Regular.otf) format('opentype');
-  }
-
   h1 {
     font-family: 'Tungsten-SemiBold', sans-serif;
     font-size: 3rem;
