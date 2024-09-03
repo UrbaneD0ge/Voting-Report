e70da7bdcb3fd9fe491db85542ca2ee9cba73c97
Kip Dunlap <kipling.dunlap@gmail.com>
2024-09-03 14:05:34 -0400

    September planners updates

--- src/routes/plannersScript[NPU]/+page.svelte
+++ src/routes/plannersScript[NPU]/+page.svelte
@@ -155,32 +155,37 @@
 </ul>
 <hr>
 <h2><b>Next, the updates from the Planning Department:</b></h2>
-<ol>
-    <li>
-    <p>We are in the final month of our “Love Thy Neighbor” phase of the NPUs’ 50th anniversary. This phase celebrated good neighbors and promoted neighborhood pride. To conclude this phase, the Department of City Planning is hosting <b>Neighborhood Spirit Week (August 12-17)</b>, a week-long celebration of the heart and soul of Atlanta - its amazing neighborhoods! Join us for a week of fun activities and events as we mark the 50th anniversary of the NPUs. For more info about this and other events, text <b>NPUATL</b> to <b>24251</b>.</p>
-    </li>
-    <li>
-    <p>The Department of City Planning is continuing to update <b>Plan_A, Atlanta’s Comprehensive Development Plan</b>. We encourage you to attend the workshop in your NPU or an adjacent NPU and visit <a href="https://www.atlantaforall.com/" target="_blank"><b>AtlantaForAll.com</b></a> to stay involved in the planning process.</p>
-    </li>
-    <li>
-    <p>As you know, the City is working to rewrite our zoning code. On May 20<sup>th</sup>, the Department of City Planning released the new <b>ATL 2.0 Zoning</b> Ordinance Module I “Discussion Draft” for public review. This module includes draft standards for Form &amp; Frontage Districts and Use Districts. You can review and comment on the draft at <u><a href="https://atlzoning.com/" target="_blank"><b>ATLZoning.com</b></a></u>.</p>
-    </li>
-    <li>
-    <p><b>NPU University</b> courses coming up are:</p>
-
+<ul>
+  <li>
+    <p>As the <b>NPU’s 50th anniversary celebration</b> continues, September marks the beginning of our final phase: Planning Atlanta Together. In this phase, we will begin discussing the future of our NPU system and how we can empower neighborhoods to strengthen the NPUs. Last month, nearly 100 Sylvan Hills residents joined us for our first Love Thy Neighbor Block Party, and we are looking forward to hosting the next one in Adamsville in NPU-H on September 14<sup>th</sup>. For more info about this and other events, text <b>NPUATL</b> to <b>24251</b>.</p>
+  </li>
+  <li>
+    <p>These next two are especially important, and in fact, it’s the reason NPUs meet every month. The Department of City Planning is continuing to update <b>Plan A: Atlanta’s Comprehensive Development Plan</b>. The plan sets forth the vision, goals, policies, and actions for growth and development in Atlanta. We will be hosting public meetings and pop-ups across the city between October and February. We’ll post schedule updates and information on how to participate at <a href='https://www.atlantaforall.com/'>AtlantaForAll.com</a>.</p>
+  </li>
+  <li>
+    <p>Of equal importance is the work we are doing to rewrite our zoning code. We released the new <b>ATL Zoning 2.0</b> Ordinance Module I “Discussion Draft” for public review on May 20<sup>th</sup>. This module includes draft standards for Form & Frontage Districts and Use Districts. Revisions based on comments received will be posted on our website in October. Module II - Development Standards will be released on December 4<sup>th</sup>. Comments on ATL Zoning 2.0 will be received on all modules through March 31<sup>st</sup> at <a href='https://atlzoning.com/'>ATLzoning.com</a>.</p>
+  </li>
+  <li>
+    <p>The <b>Atlanta City Studio</b> is celebrating its official reopening on Monday, September 30<sup>th</sup>. This time, in South Downtown on Historic Hotel Row! Please drop by anytime between 12 noon and 6:30 PM at 235 Mitchell Street SW, Suite 103.</p>
+  </li>
+  <li>
+    <p>Do you know how city design and cemetery history are related? Have you heard of the Atlanta Cemetery Network? Join the Atlanta City Studio Book Club on September 24<sup>th</sup> to discuss <a href='https://fulcolibrary.bibliocommons.com/v2/record/S171C1486426'>Over My Dead Body: Unearthing the Hidden History of America’s Cemeteries</a> and learn how city design is related to and developed around cemetery history and the Atlanta Cemetery Network’s efforts to preserve cemetery sites in the city.</p>
+  </li>
+  <li>
+    <p>DCP is partnering with the Keep Atlanta Beautiful Commission to invite you to <b>Trash Bash ATL</b>, a citywide initiative in support of World Cleanup Day. To participate, just <a href="https://adopt-a-road-atlhubgis.hub.arcgis.com/pages/trash-bash-2024">visit the website</a> and schedule a clean-up event for your community on September 20<sup>th</sup> or 21<sup>st</sup>. This friendly competition among NPUs is a fantastic opportunity to show your community spirit and contribute to a cleaner, more beautiful Atlanta.</p>
+  </li>
+  <li>
+    <p>As a reminder, <b><a href='https://www.npuatlanta.org/npuuniversity'>NPU University</a></b> courses are free to all Atlanta residents. Upcoming courses are:</p>
     <ul>
-        <li><i>Elections Integrity Clinic</i></li>
-        <li><i>Navigating Mortgage & Renter Challenges</i></li>
-        <li><i>Code Enforcement Academy</i></li>
-        <li><i>Procedures III</i></li>
-        <li><i>Community Engagement Playbook</i></li>
-        <li><i>Density Matters and Neighborhood Revitalization</i></li>
-        <li><i>Intro to Geographic Information Systems (GIS)</i></li>
-    </ul>   </li>
-    <li>
-    <p> The next meeting of the <b>Atlanta City Studio Book Club</b> will be on Tuesday, August 27<sup>th</sup> at 6:30pm details at the link in the chat.</p>
-    </li>
-</ol>
+      <li class="npuu">Code Enforcement Academy</li>
+      <li class="npuu">Parliamentary Procedures II</li>
+      <li class="npuu">Atlanta Community Engagement Playbook</li>
+      <li class="npuu">Density Matters and Neighborhood Revitalization</li>
+      <li class="npuu">Intro to Geographic Information Systems (GIS), and</li>
+      <li class="npuu">Understanding Workforce Housing and Affordability</li>
+    </ul>
+  </li>
+</ul>
 <h3>Links to post:</h3>
   <br>
   <div style="display: flex;justify-content:space-around">
@@ -208,7 +213,7 @@
     text-align: left;
     margin: 1rem 6rem;
     max-width: 1200px;
-    font-family: 'GT-Eesti-Regular';
+    font-family: Inter, 'GT-Eesti-Regular';
   }
   h2 {
     font-size: 1em;
@@ -220,6 +225,11 @@
 
   li {
     margin: 1.5em;
+  }
+
+  .npuu{
+    margin: 0;
+    margin-bottom: 0;
   }
 
   span[data-outcome='support'], span[data-outcome='Approved'] {
